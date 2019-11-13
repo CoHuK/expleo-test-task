b13e3d44ac84c24a473526f12e41247f0f891113
Pavel Strongin <pavel.s@circles.asia>
2019-11-13 19:13:38 +0800

    Fix conversion rate locator

--- features/pages/result_page.rb
+++ features/pages/result_page.rb
@@ -6,7 +6,7 @@ class ResultPage
   text_field(:amount_to, id: 'cc-amount-to')
   element(:source_currency, xpath: '//button[@data-id="sourceCurrency"]//span/div//span')
   element(:target_currency, xpath: '//button[@data-id="targetCurrency"]//span/div//span')
-  span(:conversion_rate, xpath: "//*[@class='cc__source-to-target m-t-2']/span[3]")
+  span(:conversion_rate, xpath: "//span[@class='text-success']")
 
   def currencies
     [source_currency_element.text, target_currency_element.text]
